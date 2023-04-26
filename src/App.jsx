89a2441c8d0bef33e804042110b31a9dd37c705b
Leonid Shirokov <shirokoff@me.com>
2023-04-26 02:11:40 +0200

    hiding settings on a mobile device added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import ChatForm from "./components/ChatForm/ChatForm";
+import { useState } from "react";
 
 function App() {
+    const [settings, setSettings] = useState(true);
+
     return (
         <div className="w-full h-screen bg-zinc-800 flex flex-col">
-            <Header />
+            <Header setSettings={setSettings} />
             <div className="grow w-3/4 mx-auto bg-zinc-800 px-4 py-10 md:w-11/12 overflow-auto">
-                <ChatForm />
+                <ChatForm settings={settings} />
             </div>
             <Footer />
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,24 @@
+import { GiSettingsKnobs } from "react-icons/gi";
 import logo from "./logo.png";
 
-function Header() {
+function Header({ setSettings }) {
     return (
         <div className="w-full h-14 bg-zinc-950 px-5 shrink-0">
             <div className="h-full flex justify-between items-center">
                 <h1 className="text-zinc-200">
                     <a
-                        className="hover:no-underline hover:text-zinc-500 transition-all pl-8"
+                        className="hover:no-underline hover:text-zinc-500 transition-all"
                         href="/"
                     >
                         Avadakedavra Chat
                     </a>
                 </h1>
+                <button
+                    onClick={() => setSettings((prev) => !prev)}
+                    className="2xl:hidden xl:hidden lg:hidden md:block sm:block"
+                >
+                    <GiSettingsKnobs className="text-xl" />
+                </button>
                 <img className="h-8" src={logo} />
             </div>
         </div>
